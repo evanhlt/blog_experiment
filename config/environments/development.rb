2304433c7578a213274f55d6272749e7f11544f0
Admin <admin@Mac1s-Mac-mini.local>
2016-06-02 14:00:58 -0500

    Added a comment for .env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: ENV["GMAIL_DOMAIN"],
+      domain: ENV["GMAIL_DOMAIN"], #create these variables as they stand in the .env file. Needs a GMAIL_DOMAIN, GMAIL_USERNAME, and GMAIL_PASSWORD.
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
